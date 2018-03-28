c4be99f2f26c172ea9747063997de44f1cda95bf
Dave Snowberger <git@fartherout.org>
2018-03-27 19:50:57 -0700

    added docstrings for test functions

--- challenges/ll_kth_from_end/test_ll_kth_from_end.py
+++ challenges/ll_kth_from_end/test_ll_kth_from_end.py
@@ -4,14 +4,17 @@ import pytest as pt
 l = moreBetterLL([1,2,3,4,5,6])
 
 def test_k_value_alpha():
+    """Validate exception raised when non-int passed"""
     with pt.raises(TypeError):
         assert l.kthFromEnd('a')
 
 def test_k_value_valid():
+    """Validate returns correct node when valid value passed"""
     assert l.kthFromEnd(0).val == 6
     assert l.kthFromEnd(5).val == 1
 
 def test_k_value_oor():
+    """Validate exception raised when invalid (out of range) int passed"""
     with pt.raises(IndexError):
         assert l.kthFromEnd(-1)
         assert l.kthFromEnd(6)
