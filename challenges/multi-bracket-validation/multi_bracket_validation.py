f03ae6f327b30dc3956e34386ae595133f3a9f41
Dave Snowberger <git@fartherout.org>
2018-04-04 22:42:10 -0700

    cleaned up code

--- challenges/multi-bracket-validation/multi_bracket_validation.py
+++ challenges/multi-bracket-validation/multi_bracket_validation.py
@@ -1,20 +1,5 @@
 PAIRS = {'(':')', '[':']', '{':'}'}
-'''
-def multi_bracket_validation(input, term=None):
-    for i, char in enumerate(input):
-        
-        if char in PAIRS.keys():
-            res = multi_bracket_validation(input[i+1:], PAIRS[char])
-            # if anything is false kick it all the way back up
-            if res[0] is False:
-                return res if term is not None else False
-            else:
-                return res if term is not None else True
-            i += res[1]
-        
-        if char == term:
-            return (True, i+1)
-'''
+
 def multi_bracket_validation(input):
     '''Validate grouping characters are valid in a string'''
     if type(input) is not str:
@@ -37,40 +22,3 @@ def multi_bracket_validation(input):
             return False
     
     return True
-
-
-
-
-'''
-def multi_bracket_validation(input):
-    valid = True
-    for i, char in enumerate(input):
-
-        for opener,closer in PAIRS.keys():
-            if char == opener:
-                valid = multi_bracket_validation(input[i+1:])
-            
-            if char != PAIRS[term]
-                return False
-        # If not looking for closer and find closer, set False
-        if not term and char in PAIRS.values():
-            return False
-
-        # If looking for closer...
-        if term:
-
-            # If we find the incorrect closer OR we reach the end of input, set False
-            if (char in PAIRS.values() or i == len(input) - 1) and char != PAIRS[term]:
-                return False
-
-            # If we find the closer
-            if char == PAIRS[term]:
-                return i + 1
-        
-        for opener in PAIRS.keys():    
-            if char == opener:
-                if i + 1 == len(input):
-                    return False
-
-                return multi_bracket_validation(input[i+1:], opener)
-'''
