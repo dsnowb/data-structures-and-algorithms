56babf5b7b739efdc1e9feee8875a91bc6fe0e43
Dave Snowberger <git@fartherout.org>
2018-04-05 20:34:09 -0700

    added docstrings to test file

--- challenges/towers-of-hanoi/test_towers_of_hanoi.py
+++ challenges/towers-of-hanoi/test_towers_of_hanoi.py
@@ -2,11 +2,14 @@ from towers_of_hanoi import towers_of_hanoi as toh
 import pytest
 
 def test_three():
+    '''Test moves for 3 discs'''
     assert toh(3) == ['Move Disc #0', 'Move Disc #1', 'Move Disc #0', 'Move Disc #2', 'Move Disc #0', 'Move Disc #1', 'Move Disc #0']
 
 def test_four():
+    '''Test moves for 4 discs'''
     assert toh(4) == ['Move Disc #0', 'Move Disc #1', 'Move Disc #0', 'Move Disc #2', 'Move Disc #0', 'Move Disc #1', 'Move Disc #0', 'Move Disc #3', 'Move Disc #0', 'Move Disc #1', 'Move Disc #0', 'Move Disc #2', 'Move Disc #0', 'Move Disc #1', 'Move Disc #0']
 
 def test_invalid():
+    '''Test invalid input'''
     with pytest.raises(TypeError):
         assert toh('a')
